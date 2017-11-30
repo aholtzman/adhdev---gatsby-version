d399e490fd334d7ced4d3d83b2bf74da9492e037
Adam Holtzman <adam.holtzman@gmail.com>
2017-11-30 10:13:33 -0500

    block out display area

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import '../GlobalThemes'
 import Nav from '../components/Nav'
 
+const Container = styled.div`
+  width: 80vw;
+  height: 80vh;
+  border: 2px solid green;
+  margin: auto;
+  display: flex;
+  margin-top: 60px;
+  justify-content: center;
+  align-items: center;
+`
+
+const Display = styled.div`
+  width: 100%;
+  height: 100%;
+  border: 2px solid red;
+`
+
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <Container>
     <Helmet
       title="ADH - Front End Developer"
       meta={[
@@ -15,10 +33,10 @@ const TemplateWrapper = ({ children }) => (
       ]}
     />
     <Nav />
-    <div>
+    <Display>
       {children()}
-    </div>
-  </div>
+    </Display>
+  </Container>
 )
 
 TemplateWrapper.propTypes = {

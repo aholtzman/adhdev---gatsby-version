5163c8336cc919103fd03ead7347c28d1fe43fb3
Adam Holtzman <adam.holtzman@gmail.com>
2017-12-01 09:42:03 -0500

    Bio text edit

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,7 @@ const Text = styled.div`
 const About = () => (
   <Text>
     <p>Since 2002, Adam Holtzman occasionally designed and built websites for both himself and colleagues. Recently, his focus shifted to a more professional and deeper level of web development by learning front end languages, techniques and implementations. His current projects include developing sites with React, has been utilizing CSS in JS with Styled-Components and new design layouts with CSS Grid.</p>
-    <p>Adam is a former chef, an art dealer and exhibiting artist (photography and painting). He has been known to produce photography and art books; designing, editing and intermittently publishing over fifty books since 2007.</p>
+    <p>Adam is a former chef, an art dealer and exhibiting artist (photography and painting). He has been known to produce photography and art books; designing, editing and intermittently publishing many since 2007.</p>
   </Text>
 )
 

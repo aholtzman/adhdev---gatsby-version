3036767bef5ff71d15ae3fc04e326cbfd8b7d3c3
Adam Holtzman <adam.holtzman@gmail.com>
2017-12-22 14:42:00 -0500

    add site to project list
    add in woodmann fine art to portfolio

--- src/images/data.js
+++ src/images/data.js
@@ -23,13 +23,13 @@ const data = [
 },
 {
   id: 3,
-  title: "Common Loon",
-  image: require('../images/cl.png'),
-  desc: "Website for the band Common Loon (work in progress)",
-  code: "React, React-Router, CSS Grid & Styled-Components",
-  role: "Design, Development & created the music video for 'The Coup'",
-  url: 'https://commonloon-heroku.herokuapp.com/',
-  dates: '2017 - ongoing',
+  title: "Woodmann Fine Art",
+  image: require('../images/wfa.png'),
+  desc: "Website for private fine art dealer, occasional content updates",
+  code: "Adobe Muse",
+  role: "Design & Development",
+  url: 'http://woodmannfineart.com/',
+  dates: '2014 - ongoing',
   active: true
 },
 {
@@ -56,24 +56,35 @@ const data = [
 },
 {
   id: 6,
+  title: "Common Loon",
+  image: require('../images/cl.png'),
+  desc: "Website for the band Common Loon (work in progress)",
+  code: "React, React-Router, CSS Grid & Styled-Components",
+  role: "Design, Development & created the music video for 'The Coup'",
+  url: 'https://commonloon-heroku.herokuapp.com/',
+  dates: '2017 - ongoing',
+  active: true
+},
+{
+  id: 7,
   title: "Eastport, Maine Weather",
   image: require('../images/eastport.png'),
   desc: "Code demo site (shows use of retreving and displaying data froma live API). Small web app that shows the current weather of Eastport, Maine.",
   code: "React, Styled-Components & Open Weather API",
   role: "Design & Development",
   url: 'http://eastportweather.surge.sh/',
-  dates: '2017 - ongoing',
+  dates: '2017',
   active: true
 },
 {
-  id: 7,
+  id: 8,
   title: "Dot Draw",
   image: require('../images/dots.png'),
   desc: "Code demo site (shows use of state). A simple Web App that lets users make a pointillisim drawing with clicks",
   code: "React & Styled-Components",
   role: "Design & Development",
   url: 'https://dot-draw.herokuapp.com',
-  dates: '2017 - ongoing',
+  dates: '2017',
   active: true
 }
 ]

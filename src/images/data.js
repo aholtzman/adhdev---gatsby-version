72846236c0866940ca2c6648765ec162705a57b8
Adam Holtzman <adam.holtzman@gmail.com>
2017-12-22 14:32:25 -0500

    add data fileds for more detailed web project descriptions

--- src/images/data.js
+++ src/images/data.js
@@ -1,21 +1,25 @@
 const data = [
   {
   id: 1,
+  title: "Adam Holtzman Photography & Artworks",
+  image: require('../images/adh.png'),
+  desc: "Online art portfolio site, new design and build completed every 12 to 18 months",
+  code: "React, React-Router, CSS Grid & Styled-Components",
+  role: "Design & Development",
+  url: 'http://www.adamholtzman.com',
+  dates: '2002 - ongoing',
+  active: true
+},
+{
+  id: 2,
   title: "David Gremp",
   image: require('../images/gremp.png'),
   desc: "Website for the fine art photographer David Gremp",
   code: "React, Gatsby, CSS Grid & Styled-Components",
   role: "Design & Development",
-  url: 'http://davidgrempphotos.com/'
-},
-{
-  id: 2,
-  title: "Alibi Fine Art",
-  image: require('../images/alibi.png'),
-  desc: "Legacy website for a now closed Art Gallery",
-  code: "React, React-Router & Styled-Components",
-  role: "Design & Development",
-  url: 'http://www.alibifineart.com'
+  url: 'http://davidgrempphotos.com/',
+  dates: '2017 - ongoing',
+  active: true
 },
 {
   id: 3,
@@ -24,7 +28,9 @@ const data = [
   desc: "Website for the band Common Loon (work in progress)",
   code: "React, React-Router, CSS Grid & Styled-Components",
   role: "Design, Development & created the music video for 'The Coup'",
-  url: 'https://commonloon-heroku.herokuapp.com/'
+  url: 'https://commonloon-heroku.herokuapp.com/',
+  dates: '2017 - ongoing',
+  active: true
 },
 {
   id: 4,
@@ -33,34 +39,42 @@ const data = [
   desc: "This website",
   code: "React, Gatsby & Styled-Components",
   role: "Design & Development",
-  url: 'http://www.adamdavidholtzman.com'
+  url: 'http://www.adamdavidholtzman.com',
+  dates: '2017 - ongoing',
+  active: true
 },
 {
   id: 5,
-  title: "Adam Holtzman",
-  image: require('../images/adh.png'),
-  desc: "Latest iteration of my personal website featuring my artwork, since 2002",
-  code: "React, React-Router, CSS Grid & Styled-Components",
+  title: "Alibi Fine Art",
+  image: require('../images/alibi.png'),
+  desc: "Legacy website for a now closed Art Gallery",
+  code: "React, React-Router & Styled-Components",
   role: "Design & Development",
-  url: 'http://www.adamholtzman.com'
+  url: 'http://www.alibifineart.com',
+  dates: '2010 - 2017',
+  active: true
 },
 {
   id: 6,
   title: "Eastport, Maine Weather",
   image: require('../images/eastport.png'),
-  desc: "A simple Web App that shows the current weather in the eastern most US city.",
+  desc: "Code demo site (shows use of retreving and displaying data froma live API). Small web app that shows the current weather of Eastport, Maine.",
   code: "React, Styled-Components & Open Weather API",
   role: "Design & Development",
-  url: 'http://eastportweather.surge.sh/'
+  url: 'http://eastportweather.surge.sh/',
+  dates: '2017 - ongoing',
+  active: true
 },
 {
   id: 7,
   title: "Dot Draw",
   image: require('../images/dots.png'),
-  desc: "A simple Web App that lets users make a pointillisim drawing with clicks",
+  desc: "Code demo site (shows use of state). A simple Web App that lets users make a pointillisim drawing with clicks",
   code: "React & Styled-Components",
   role: "Design & Development",
-  url: 'https://dot-draw.herokuapp.com'
+  url: 'https://dot-draw.herokuapp.com',
+  dates: '2017 - ongoing',
+  active: true
 }
 ]
 

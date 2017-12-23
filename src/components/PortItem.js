b8dfdd96fa2ed179e016fb359bcaa36fae846abc
Adam Holtzman <adam.holtzman@gmail.com>
2017-12-23 14:01:26 -0500

    add division line to web portfolio, seperate title & dates

--- src/components/PortItem.js
+++ src/components/PortItem.js
@@ -17,6 +17,7 @@ class PortItem extends Component {
           />
           <TextWrap>
           <Title>{this.props.data.title}</Title>
+          <hr></hr>
           <h2>{this.props.data.dates}</h2>
           <hr></hr>
           <br></br>
